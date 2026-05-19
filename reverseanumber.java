e7eeb90419429112302c1e60755d8101b13e4e0f
Yashita Gaur <yashitagaur72@gmail.com>
2026-05-19 20:00:10 +0530

    Added reverse a number program

---
+++ reverseanumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class reverseanumber {
+         public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter a number:");
+            int n=sc.nextInt();
+            int dig;
+            int rev=0;
+            while(n>0){
+                dig=n%10;
+                rev=rev*10+dig;
+                n=n/10;
+            }
+            System.out.println(rev);
+         }
+
+
+    
+}
