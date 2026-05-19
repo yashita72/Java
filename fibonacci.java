b3d3e34f26e6fa0300e675f3e30651e099533240
Yashita Gaur <yashitagaur72@gmail.com>
2026-05-19 19:58:19 +0530

    Added fibonacci program

---
+++ fibonacci.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class fibonacci {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n=sc.nextInt();
+        int a=0;
+        int b=1;
+        int c;
+        System.out.println("Fibonacci Series:");
+        System.out.print(a + " " + b+" ");
+        for(int i =2;i<n;i++)
+        {
+             c=a+b;
+             a=b;
+             b=c;
+            System.out.print(c + " " );  
+        }
+    }
+}
